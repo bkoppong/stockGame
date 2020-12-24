117a584f6901e4ee3a308041ffdbea8009c7d958
Brandon Oppong-Antwi <55814612+bkoppong@users.noreply.github.com>
2020-12-24 02:00:20 -0500

    Delete character.py

--- character.py
+++
@@ -1,6 +0,0 @@
-class BusinessPerson():
-    def __init__(self, name):
-        self.name = name
-        self.money = 1000
-        print("Person name : " + self.name.title())
-        print("Person money : $" + str(self.money).title())
